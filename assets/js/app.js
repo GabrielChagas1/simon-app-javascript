52f0e3d9c3b6c526a95a5859a943a2b1ce553f11
Gabriel Chagas <gabrielserq@outlook.com>
2022-09-21 09:03:51 -0300

    chore: creating variable result.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,4 @@ const countValue = document.getElementById("count");
 const colorPart = document.querySelectorAll(".color-part");
 const container = document.querySelector(".container");
 const startButton = document.querySelector("#start");
+const result = document.querySelector("#result");
