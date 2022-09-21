2a5c0d2edb266ba1376651b1d598b1c5007fd0c8
Gabriel Chagas <gabrielserq@outlook.com>
2022-09-21 09:04:45 -0300

    chore: mapping colors by creating colors object

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,22 @@ const container = document.querySelector(".container");
 const startButton = document.querySelector("#start");
 const result = document.querySelector("#result");
 const wrapper = document.querySelector(".wrapper");
+//Mapping Colors By Creating Colors Object
+const colors = {
+  color1: {
+    current: "#068e06",
+    new: "#11e711",
+  },
+  color2: {
+    current: "#950303",
+    new: "#fd2a2a",
+  },
+  color3: {
+    current: "#01018a",
+    new: "#2062fc",
+  },
+  color4: {
+    current: "#919102",
+    new: "#fafa18",
+  },
+};
