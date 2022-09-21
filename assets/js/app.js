1682997eda55b00da2fdc79a12cec6a4dad49a16
Gabriel Chagas <gabrielserq@outlook.com>
2022-09-21 09:03:38 -0300

    chore: creating variable startButton.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,4 @@
 const countValue = document.getElementById("count");
 const colorPart = document.querySelectorAll(".color-part");
 const container = document.querySelector(".container");
+const startButton = document.querySelector("#start");
